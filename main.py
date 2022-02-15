adf4d6a548b6fcd43475a474028552bbaaa433ba
joy00000 <57339643+joy00000@users.noreply.github.com>
2022-02-15 11:37:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+from flask import request, render_template
+import joblib
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+@app.route("/", methods = ["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        sugar = request.form.get("sugar")
+        milk = request.form.get("milk")
+        #print(sugar, milk)
+        model = joblib.load("CTaste")
+        pred = model.predict([[sugar, milk]])
+        #print(pred)
+        pred = pred[0]
+        
+        if pred == 1:
+            res = "good!"
+        else:
+            res = "bad :("
+            
+        s = ("Predicted taste is: " + res)
+        return (render_template("index.html", result = s))
+    else: 
+        return (render_template("index.html", result = "Predict Chocolate Taste"))
+
+
+# In[4]:
+
+
+app.run()
+
+
+# In[ ]:
+
+
+
+
